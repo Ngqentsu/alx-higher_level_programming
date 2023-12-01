c34dfe0a38df4adc416e34bdedcd0647ab3a806e
Simbonge Ngqentsu <ngqentsu0307@gmail.com>
2023-12-01 04:59:02 -0800

    Python script that takes in a URL, sends a request to the URL and displays the body of the response

---
+++ 0x11-python-network_1/7-error_code.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+""" Python script that takes in a URL,
+    sends a request to the URL and
+    displays the body of the response (decoded in utf-8).
+"""
+
+import requests
+from sys import argv
+
+
+if __name__ == "__main__":
+    try:
+        with requests.get(argv[1]) as response:
+            print(response.text)
+            if response.status_code >= 400:
+                print("Error code: {}".format(response.status_code))
+    except requests.exceptions.HTTPError as e:
+        print("Error code: {}".format(e.response.status_code))
