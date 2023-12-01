f06455abbd0b8f542f1f2905792c6dd66f041eb4
Simbonge Ngqentsu <ngqentsu0307@gmail.com>
2023-12-01 05:51:21 -0800

    Python script that takes in a letter and sends a POST request to http://0.0.0.0:5000/search_user with the letter as a parameter

---
+++ 0x11-python-network_1/8-json_api.py
@@ -0,0 +1,28 @@
+#!/usr/bin/python3
+"""Python script that takes in a letter and sends a POST request
+   to http://0.0.0.0:5000/search_user with the letter as a parameter.
+"""
+
+import requests
+import sys
+
+
+if __name__ == "__main__":
+    url = "'http://0.0.0.0:5000/search_user"
+    if len(sys.argv) == 1:
+        q = ""
+    else:
+        q = sys.argv[1]
+    data = {'q': q}
+    try:
+        response = requests.post(url, data=data)
+        try:
+            json_res = response.json()
+            if json_res:
+                print("[{}] {}".format(json_res.get("id"), json_res.get("name")))
+            else:
+                print("No result")
+        except ValueError:
+            print("Not a valid JSON")
+    except requests.RequestException as e:
+        pass
