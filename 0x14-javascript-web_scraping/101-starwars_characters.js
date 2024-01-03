d460baac00bb7740361e0ad58e70caab44a6e993
Simbonge Ngqentsu <ngqentsu0307@gmail.com>
2024-01-03 13:50:23 -0800

    A script that prints all characters of a Star Wars movie

---
+++ 0x14-javascript-web_scraping/101-starwars_characters.js
@@ -0,0 +1,44 @@
+#!/usr/bin/node
+// Prints all characters of a Star Wars movie
+
+// Import the 'request' module
+const request = require('request');
+
+// Get the movie ID from the command line arguments
+const movieId = process.argv[2];
+
+// Function to fetch data from the Star Wars API
+function fetchData (url) {
+  return new Promise((resolve, reject) => {
+    // Make an HTTP GET request to the provided URL
+    request(url, (error, response, body) => {
+      if (error) {
+        reject(error);
+      } else {
+        resolve(body);
+      }
+    });
+  });
+}
+
+// Function to print all characters of a Star Wars movie
+async function printCharacters (movieId) {
+  try {
+    const apiUrl = `https://swapi-api.alx-tools.com/api/films/${movieId}/`;
+    const movieData = await fetchData(apiUrl);
+    const movie = JSON.parse(movieData);
+
+    // Display one character name by line in the same order as the 'characters' list in the /films/ response
+    for (let i = 0; i < movie.characters.length; i++) {
+      const characterUrl = movie.characters[i];
+      const characterData = await fetchData(characterUrl);
+      const character = JSON.parse(characterData);
+      console.log(character.name);
+    }
+  } catch (error) {
+    console.error(`Error: ${error}`);
+  }
+}
+
+// Call the function with the provided movie ID
+printCharacters(movieId);
