ba2dd42b8f95c5c14088b97248ee0aa3d5da3bd0
Simbonge Ngqentsu <ngqentsu0307@gmail.com>
2024-01-10 13:19:16 -0800

    jQuery

--- 0x15-javascript-web_jquery/102-script.js
+++ 0x15-javascript-web_jquery/102-script.js
@@ -2,7 +2,11 @@ $('document').ready(function () {
   const url = 'https://www.fourtonfish.com/hellosalut/?';
 
   $('INPUT#btn_translate').click(function () {
-    $.get(`${url}lang=${$('INPUT#language_code').val()}`, function (data) {
+    const languageCode = $('INPUT#language_code').val();
+    console.log('Language Code:', languageCode);
+
+    $.get(`${url}lang=${languageCode}`, function (data) {
+      console.log('API Response:', data);
       $('DIV#hello').html(data.hello);
     });
   });
